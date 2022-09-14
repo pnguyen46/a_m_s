aea99dd4ec0bc7917989c7588106adb4b991b20d
Tron Nguyen <pnguye894@gmail.com>
2022-09-14 12:57:17 -0400

    Fix add and edit features

--- public/js/main.js
+++ public/js/main.js
@@ -24,110 +24,175 @@
 //     console.error(error);
 //   }
 // });
-const editBtn = document.querySelectorAll('.btn.editBtn');
-Array.from(editBtn).forEach((el)=>{
-  el.addEventListener('click', editItem)
-})
 
-const addInputBoxBtn = document.getElementById('addInputBox');
-addInputBoxBtn.addEventListener('click',addInputBox);
+const editBtn = document.querySelectorAll(".btn.editBtn");
+Array.from(editBtn).forEach((el) => {
+  el.addEventListener("click", editItem);
+});
 
-const deleteInputBoxBtn = document.getElementById('deleteInputBox');
-deleteInputBoxBtn.addEventListener('click',deleteInputBox)
+// const addInputBoxBtn = document.getElementById('addInputBox');
+// addInputBoxBtn.addEventListener('click',addInputBox);
 
-function setModalContent(modalType,data){
+// const deleteInputBoxBtn = document.getElementById('deleteInputBox');
+// deleteInputBoxBtn.addEventListener('click',deleteInputBox)
+
+function setModalContent(modalType, data) {
   switch (modalType) {
-    case 'customer':
-      document.getElementById('editCustomerName').setAttribute('value',data.name);
-      document.getElementById('editCustomerAddr').setAttribute('value',data.address);
-      document.getElementById('editCustomerPhone').setAttribute('value',data.phone_number);
-      document.getElementById('editCustomerFavTech').setAttribute('value',data.fav_tech);
-      document.getElementById('customerEditForm').setAttribute('action',`/${modalType}/editItem/${data._id}?_method=PUT`)
+    case "customer":
+      document
+        .getElementById("editCustomerName")
+        .setAttribute("value", data.name);
+      document
+        .getElementById("editCustomerAddr")
+        .setAttribute("value", data.address);
+      document
+        .getElementById("editCustomerPhone")
+        .setAttribute("value", data.phone_number);
+      document
+        .getElementById("editCustomerFavTech")
+        .setAttribute("value", data.fav_tech);
+      document
+        .getElementById("customerEditForm")
+        .setAttribute(
+          "action",
+          `/${modalType}/editItem/${data._id}?_method=PUT`
+        );
       break;
-    case 'inventory':
-      document.getElementById('editName').setAttribute('value',data.name);
-      document.getElementById('editQty').setAttribute('value',data.quantity);
-      document.getElementById('editBrand').setAttribute('value',data.brand);
-      document.getElementById('editLocation').setAttribute('value',data.location);
-      document.getElementById('editForm').setAttribute('action',`/${modalType}/editItem/${data._id}?_method=PUT`)
+    case "inventory":
+      document.getElementById("itemName").setAttribute("value", data.name);
+      document.getElementById("itemQty").setAttribute("value", data.quantity);
+      document.getElementById("itemBrand").setAttribute("value", data.brand);
+      document
+        .getElementById("itemLocation")
+        .setAttribute("value", data.location);
+      document
+        .getElementById("form")
+        .setAttribute(
+          "action",
+          `/${modalType}/editItem/${data._id}?_method=PUT`
+        );
       break;
-    case 'employee':
-      document.getElementById('editEmployeeName').setAttribute('value',data.name);
-      document.getElementById('editEmployeeAddr').setAttribute('value',data.address);
-      document.getElementById('editEmployeePhone').setAttribute('value',data.phone_number);
-      document.getElementById('editEmployeeRepair').setAttribute('value',data.repair);
-      document.getElementById('editEmployeeSpecialty').setAttribute('value',data.specialty);
-      document.getElementById('editEmployeeClass').setAttribute('value',data.class);
-      document.getElementById('editEmployeeStatus').setAttribute('value',data.status);
-      document.getElementById('employeeEditForm').setAttribute('action',`/${modalType}/editItem/${data._id}?_method=PUT`)
+    case "employee":
+      document
+        .getElementById("editEmployeeName")
+        .setAttribute("value", data.name);
+      document
+        .getElementById("editEmployeeAddr")
+        .setAttribute("value", data.address);
+      document
+        .getElementById("editEmployeePhone")
+        .setAttribute("value", data.phone_number);
+      document
+        .getElementById("editEmployeeRepair")
+        .setAttribute("value", data.repair);
+      document
+        .getElementById("editEmployeeSpecialty")
+        .setAttribute("value", data.specialty);
+      document
+        .getElementById("editEmployeeClass")
+        .setAttribute("value", data.class);
+      document
+        .getElementById("editEmployeeStatus")
+        .setAttribute("value", data.status);
+      document
+        .getElementById("employeeEditForm")
+        .setAttribute(
+          "action",
+          `/${modalType}/editItem/${data._id}?_method=PUT`
+        );
       break;
-    case 'ticket':
-      document.getElementById('repairEditType').setAttribute('value',data.repairType);
-      document.getElementById('repairEditHrs').setAttribute('value',data.hour);
-      document.getElementById('repairEditParts').setAttribute('value',data.parts);
-      document.getElementById('repairEditCost').setAttribute('value',data.cost);
-      document.getElementById('repairEditDate').setAttribute('value',new Date(data.date).toDateString());
-      document.getElementById('repairEditTech').setAttribute('value',data.technician);
-      document.getElementById('repairEditStatus').setAttribute('value',data.status);
-      document.getElementById('repairEditModal').setAttribute('action',`/${modalType}/editItem/${data._id}?_method=PUT`)
+    case "ticket":
+      document
+        .getElementById("repairEditType")
+        .setAttribute("value", data.repairType);
+      document.getElementById("repairEditHrs").setAttribute("value", data.hour);
+      document
+        .getElementById("repairEditParts")
+        .setAttribute("value", data.parts);
+      document
+        .getElementById("repairEditCost")
+        .setAttribute("value", data.cost);
+      document
+        .getElementById("repairEditDate")
+        .setAttribute("value", new Date(data.date).toDateString());
+      document
+        .getElementById("repairEditTech")
+        .setAttribute("value", data.technician);
+      document
+        .getElementById("repairEditStatus")
+        .setAttribute("value", data.status);
+      document
+        .getElementById("repairEditModal")
+        .setAttribute(
+          "action",
+          `/${modalType}/editItem/${data._id}?_method=PUT`
+        );
       break;
-  
+
     default:
       break;
   }
 }
 
-async function editItem(event){
+async function editItem(event) {
   try {
-    const itemId = event.target.getAttribute('data-bs-id');
-    const route = event.target.getAttribute('data-bs-identifier');
+    const form = document.getElementById('form');
+    const itemId = event.target.getAttribute("data-bs-id");
+    const route = event.target.getAttribute("data-bs-identifier");
     const response = await fetch(`${route}/${itemId}`, {
-      method: 'GET'
+      method: "GET",
     });
-    const {item} = await response.json();
+    const { item } = await response.json();
     const [data] = item;
-    console.log(data);
-    setModalContent(route,data);
+    form.reset();
+    setModalContent(route, data);
   } catch (error) {
     return console.error(error);
   }
 }
 
 function setAttributes(element, attributes) {
-  Object.keys(attributes).forEach(attr => {
+  Object.keys(attributes).forEach((attr) => {
     element.setAttribute(attr, attributes[attr]);
   });
 }
 
-function addInputBox(){
+function addInputBox() {
   const formEle = addInputBoxBtn.parentElement.previousElementSibling;
-  const container = document.createElement('div');
-  container.setAttribute('class','d-flex');
+  const container = document.createElement("div");
+  container.setAttribute("class", "d-flex");
 
-  const partNameInputBox = document.createElement('input');
+  const partNameInputBox = document.createElement("input");
   const partNameAttributes = {
-    type: 'text',
-    class:'form-control w-75',
-    name: 'repairParts',
+    type: "text",
+    class: "form-control w-75",
+    name: "repairParts",
   };
-  setAttributes(partNameInputBox,partNameAttributes);
+  setAttributes(partNameInputBox, partNameAttributes);
 
-  const partQtyInputBox = document.createElement('input');
+  const partQtyInputBox = document.createElement("input");
   const partQtyAttributes = {
-    type: 'number',
-    class:'form-control w-25',
-    name: 'repairParts',
+    type: "number",
+    class: "form-control w-25",
+    name: "repairParts",
   };
-  setAttributes(partQtyInputBox,partQtyAttributes);
+  setAttributes(partQtyInputBox, partQtyAttributes);
   container.appendChild(partNameInputBox);
   container.appendChild(partQtyInputBox);
   formEle.appendChild(container);
 }
 
+const resetAddBtn = document.getElementById("resetAddBtn");
+resetAddBtn.addEventListener("click", (event) => {
+  document.getElementById("resetBtn").setAttribute("data-bs-type", "edit");
+});
 
-function deleteInputBox(){
-  const formEl = document.getElementById('partContainer');
-  if(formEl.children.length > 1){
-    formEl.removeChild(formEl.lastElementChild);
+const resetBtn = document.getElementById("resetBtn");
+resetBtn.addEventListener("click", (event) => {
+  const flag = event.target.getAttribute("data-bs-type");
+  if (flag === "edit") {
+    const input = document.querySelectorAll("#form input");
+    input.forEach(item => item.value = '');
+    resetBtn.setAttribute("data-bs-type", "");
   }
-}
+});
